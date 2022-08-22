7c53f78ccdf710b094e27cf4e6c1346598b942e2
82orez <82orez@naver.com>
2022-08-22 22:29:46 +0900

    8/22 오후

---
+++ js/book.js
@@ -0,0 +1,25 @@
+function Book(title, author, volume, price) {
+    this.title = title;
+    this.author = author;
+    this. volume = volume;
+    this.price = price;
+    this.printPrice = `이 책의 가격은 ${price}원 입니다.`;
+}
+
+let html = new Book("웹표준의 정석", "고승원", "608", "25000");
+let youtube = new Book("유튜브 영상 만들기", "김씨", "368", "16000");
+let python = new Book("점프 투 파이썬", "박씨", "352", "18000");
+
+let bookList = [html, youtube, python];
+
+document.write("<h1>책 제목으로 살펴보기</h1>")
+for (let i=0; i < bookList.length; i++) {
+    document.write(`<p>${bookList[i].title}</p>`);
+}
+document.write('<br>');
+
+document.write(python.price);
+document.write('<br>');
+
+document.write(python.printPrice);
+
