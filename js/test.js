2573c49742449d3309705dadd24b0f966ab4777d
82orez <82orez@naver.com>
2022-09-02 16:46:09 +0900

    9/2일 오후

--- js/test.js
+++ js/test.js
@@ -1,31 +1,32 @@
 var itemList = [];
 
-var addBtn = document.querySelector('#add');
-addBtn.addEventListener('click', addList); // addBtn.onclick = addList; 라고 해도 됨
+var addBtn = document.querySelector("#add");
+addBtn.addEventListener("click", addList); // addBtn.onclick = addList; 라고 해도 됨
 
 function addList() {
-    var item = document.querySelector('#item').value; // 텍스트 필드 내용 가져옴
+    var item = document.querySelector("#item").value; // 텍스트 필드 내용 가져옴
     if (item != null) {
         itemList.push(item); // itemList 배열의 끝에 item 변수 값 추가
-        document.querySelector('#item').value = ''; // id=”item”인 요소의 값을 지움
-        document.querySelector('#item').focus(); // 텍스트 필드에 focus( ) 메서드 적용
+        document.querySelector("#item").value = ""; // id=”item”인 요소의 값을 지움
+        document.querySelector("#item").focus(); // 텍스트 필드에 focus( ) 메서드 적용
     }
     showList();
 }
 
 function showList() {
-    var list = '<ul>'; // 목록을 시작하는 <ul> 태그 저장
+    var list = "<ul>"; // 목록을 시작하는 <ul> 태그 저장
     for (var i = 0; i < itemList.length; i++) {
         // 배열 요소마다 반복
-        list += '<li>' + itemList[i] + "<span class='close' id=" + i + '>X</span></li>'; // 요소와 삭제 버튼을 <li>~</li>로 묶음
+        list += "<li>" + itemList[i] + "<span class='close' id=" + i + ">X</span></li>";
+        // 요소와 삭제 버튼을 <li>~</li>로 묶음
     }
-    list += '</ul>'; // 목록을 끝내는 </ul> 태그 저장
+    list += "</ul>"; // 목록을 끝내는 </ul> 태그 저장
 
-    document.querySelector('#itemList').innerHTML = list; // list 내용 표시
+    document.querySelector("#itemList").innerHTML = list; // list 내용 표시
 
-    var remove = document.querySelectorAll('.close'); // 삭제 버튼을 변수로 저장. 배열 형태가 됨
+    var remove = document.querySelectorAll(".close"); // 삭제 버튼을 변수로 저장. 배열 형태가 됨
     for (var i = 0; i < remove.length; i++) {
         // remove 배열의 요소 모두를 확인
-        remove[i].addEventListener('click', removeList); // 요소를 클릭하면 removeList() 실행
+        remove[i].addEventListener("click", removeList); // 요소를 클릭하면 removeList() 실행
     }
 }
