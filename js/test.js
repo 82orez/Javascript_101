d9c97dc7ecc5dddcda59bc5dfcab0cb4add6e673
82orez <82orez@naver.com>
2022-10-21 07:30:48 +0900

    10/20

--- js/test.js
+++ js/test.js
@@ -1,10 +1,18 @@
-function doSomething(add) {
-   console.log(add);
-   console.log(add(3,4));
-}
+// for (let i = 2; i < 10 ; i++) {
+//     for (let j = 1; j < 10; j++) {
+//         console.log(`${i} X ${j} = ${i * j}`);
+//     }
+// }
 
-function add(a, b) {
-  return a + b;
-}
+// console.log(typeof 1);
+// console.log(typeof '1');
+// console.log(typeof (1 < 2));
+// console.log((1 > 2));
 
-doSomething(add);
+// function triangleArea(base, height) {
+//     return base * height / 2;
+// }
+
+let triangleArea = (base, height) => base * height / 2;
+
+console.log(triangleArea(5,4));
