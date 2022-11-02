59d902fed883566362f19152e5d1b6d657b0f7e7
82orez <82orez@naver.com>
2022-11-03 07:58:05 +0900

    폴더 정리

--- UrClass/Section_01/00_Sprint/fe-sprint-cli-practice-main/getListMultiplesOfTwo.js
+++ UrClass/Section_01/00_Sprint/fe-sprint-cli-practice-main/getListMultiplesOfTwo.js
@@ -2,7 +2,7 @@ const { range } = require('range'); // range 모듈을 불러옵니다
 
 function getListMultiplesOfTwo(upTo) {
   // TODO
-    return range(2, upTo+2,2);
+    return range(2, upTo + 2, 2);
 }
 
 module.exports = getListMultiplesOfTwo;
