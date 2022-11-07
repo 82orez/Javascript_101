9d56d0a3d06bba1253c7da090fac9d1f34d71382
82orez <82orez@naver.com>
2022-11-07 15:44:39 +0900

    ES6

--- UrClass/Section_01/07_ES6/spread_rest.js
+++ UrClass/Section_01/07_ES6/spread_rest.js
@@ -8,8 +8,23 @@ function spreadSum(x, y, z) {
 const numbers = [1, 2, 3];
 console.log('Spread', spreadSum(...numbers)); // Array -> String
 
+console.log(Math.max(...numbers));
+
 // 2. rest 문법
 // ** 파라미터를 배열의 형태로 받아서 사용할 수 있습니다. 파라미터 개수가 가변적일 때 유용합니다.
+
+function restMap(...theArgs) {
+    return theArgs.map((value, index, array) => {
+        if (value % 2 === 0) {
+            return `Index ${index} 의 값 ${value} 은 짝수`;
+        }
+        return `Index ${index} 의 값 ${value} 은 홀수`;
+    });
+}
+
+
+console.log('Rest', restMap(1, 2, 3));
+console.log('Rest', restMap(1, 2, 3, 4));
 
 function sum(...theArgs) {
     return theArgs.reduce((previous, current) => {
