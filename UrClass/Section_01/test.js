4df5ab77ca6e901f321955eb139bf0955520f24d
82orez <82orez@naver.com>
2022-11-08 10:29:58 +0900

    koans 스코프 까지

--- UrClass/Section_01/test.js
+++ UrClass/Section_01/test.js
@@ -1,45 +1,78 @@
-// 1. 배열 합치기
+console.log(1+'1');
+console.log(typeof (1 +'1'));
 
-let parts = ['shoulders', 'knees'];
-let lyrics = ['head', ...parts, 'and', 'toes'];
-let lyrics1 = ['head', parts, 'and', 'toes'];
+console.log(1 + true);
+console.log(typeof (1 + true));
 
-console.log(lyrics);
-console.log(lyrics1);
+console.log('1' + true);
+console.log(typeof ('1' + true));
 
-let arr1 = [0, 1, 2];
-let arr2 = [3, 4, 5];
+let foo = 1;
 
-arr1 = [...arr1, ...arr2];
-console.log(arr1);
-
-// 2. 배열 복사
-
-let arr = [1, 2, 3];
-let arr3 = [...arr]; // arr.slice() 와 유사
-// 참고: spread 문법은 기존 배열을 변경하지 않으므로(immutable), arr2를 수정한다고, arr 이 바뀌지 않습니다.
-arr3.push(4);
-// 질문: arr 와 arr2의 값은 각각 무엇인가요?
-console.log(arr);
-console.log(arr3);
-
-// 3. 객체에서 사용
-
-let obj1 = { foo: 'bar', x: 42 };
-let obj2 = { foo: 'baz', y: 13 };
-
-let clonedObj = { ...obj1 };
-let mergedObj = { ...obj1, ...obj2 };
-
-console.log(clonedObj);
-console.log(mergedObj);
-
-// 4. 함수에서 나머지 파라미터 받아오기
-
-function myFun(a, b, ...manyMoreArgs) {
-    console.log("a", a);
-    console.log("b", b);
-    console.log("manyMoreArgs", manyMoreArgs);
+{
+    // foo ++;
+    let foo = 2;
+    console.log(foo);
 }
 
-console.log(myFun("one", "two", "three", "four", "five", "six"));
+let funcExpressed = 'to be a function';
+
+console.log(typeof funcExpressed);
+console.log(typeof funcDeclared);
+
+function funcDeclared() {
+    return 'this is a function declaration';
+}
+
+funcExpressed = function () {
+    return 'this is a function expression';
+};
+
+console.log(typeof funcExpressed);
+
+
+// ----------------------------
+function defaultParameter(num = 5) {
+    return num;
+}
+
+console.log(defaultParameter());
+console.log(defaultParameter(10));
+
+
+// ------------------------------
+let age = 27;
+let name = 'jin';
+let height = 179;
+
+function outerFn() {
+    let age = 24;
+    name = 'jimin';
+    let height = 178;
+
+    function innerFn() {
+        age = 26;
+        let name = 'suga';
+        return height;
+    }
+
+    innerFn();
+
+    console.log(age);
+    console.log(name);
+    // expect(age).to.equal(26);
+    // expect(name).to.equal('suga');
+
+    return innerFn;
+}
+
+const innerFn = outerFn();
+
+console.log(age);
+console.log(name);
+console.log(innerFn());
+
+
+// expect(age).to.equal(27);
+// expect(name).to.equal('jimin');
+// expect(innerFn()).to.equal(178);
