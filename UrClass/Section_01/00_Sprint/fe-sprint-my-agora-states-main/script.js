4a45442eec4049ea89d07236578a67c5eb4f8bd2
82orez <82orez@naver.com>
2022-11-14 12:46:57 +0900

    아고라 1차 목업 완성

--- UrClass/Section_01/00_Sprint/fe-sprint-my-agora-states-main/script.js
+++ UrClass/Section_01/00_Sprint/fe-sprint-my-agora-states-main/script.js
@@ -66,3 +66,10 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+// 새 데이터 배열에 추가하기
+const submit = document.querySelector('.form__submit');
+const inputName = document.querySelector('#name').value;
+const inputTitle = document.querySelector('#title').value;
+const inputStory = document.querySelector('#story').value;
