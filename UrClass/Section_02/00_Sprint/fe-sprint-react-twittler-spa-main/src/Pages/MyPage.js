d876de5e7baa9532979474506be8e4a21f0a35a6
82orez <82orez@naver.com>
2022-11-28 11:06:40 +0900

    last mission 01

--- UrClass/Section_02/00_Sprint/fe-sprint-react-twittler-spa-main/src/Pages/MyPage.js
+++ UrClass/Section_02/00_Sprint/fe-sprint-react-twittler-spa-main/src/Pages/MyPage.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { dummyTweets } from "../static/dummyData";
-import "./MyPage.css";
+import React from 'react';
+import { dummyTweets } from '../static/dummyData';
+import './MyPage.css';
 // ! 위 코드는 수정하지 않습니다.
 
 // TODO - import문을 이용하여 Footer 컴포넌트를 불러옵니다.
+import Footer from '../Footer';
 
 const MyPage = () => {
   // TODO - filter 메소드를 이용하여 username이 kimcoding인 요소만 있는 배열을 filteredTweet에 할당합니다.
@@ -17,29 +18,28 @@ const MyPage = () => {
             <img src={filteredTweets[0].picture} />
           </div>
           <div className="myInfo__detail">
-            <p className="myInfo__detailName">
-              {filteredTweets[0].username} Profile
-            </p>
+            <p className="myInfo__detailName">{filteredTweets[0].username} Profile</p>
             <p>28 팔로워 100 팔로잉</p>
           </div>
         </div>
       </div>
       <ul className="tweets__mypage">
         {/* TODO : dummyTweets중 kimcoding 이 작성한 트윗 메세지만 있어야 합니다. */}
-        <li className="tweet" key={""}>
+        <li className="tweet" key={''}>
           <div className="tweet__profile">
-            <img src={""} />
+            <img src={''} />
           </div>
           <div className="tweet__content">
             <div className="tweet__userInfo">
-              <span className="tweet__username">{""}</span>
-              <span className="tweet__createdAt">{""}</span>
+              <span className="tweet__username">{''}</span>
+              <span className="tweet__createdAt">{''}</span>
             </div>
-            <div className="tweet__message">{""}</div>
+            <div className="tweet__message">{''}</div>
           </div>
         </li>
       </ul>
-      TODO : Footer 컴포넌트를 작성합니다.
+      {/*TODO : Footer 컴포넌트를 작성합니다.*/}
+      <Footer />
     </section>
   );
 };
--- UrClass/Section_02/00_Sprint/fe-sprint-react-twittler-spa-main/src/Sidebar.js
+++ UrClass/Section_02/00_Sprint/fe-sprint-react-twittler-spa-main/src/Sidebar.js
@@ -7,8 +7,8 @@ const Sidebar = () => {
     <section className="sidebar">
       {/* TODO : About 메뉴 아이콘과 Mypage 메뉴 아이콘을 작성하고 Link 컴포넌트를 이용하여 경로(path)를 연결합니다. */}
       <Link to={'/'}><i className="far fa-comment-dots"></i></Link>
-      <Link to={'/mypage'}><i className="far fa-question-circle"></i></Link>
-      <Link to={'/about'}><i className="far fa-user"></i></Link>
+      <Link to={'/about'}><i className="far fa-question-circle"></i></Link>
+      <Link to={'/mypage'}><i className="far fa-user"></i></Link>
     </section>
   );
 };
