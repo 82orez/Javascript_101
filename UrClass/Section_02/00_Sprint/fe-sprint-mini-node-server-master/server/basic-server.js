a2bb48d19ae316b57e10bbaf9ebdbe5c75eecc39
82orez <82orez@naver.com>
2022-12-08 11:42:57 +0900

    spr node ing

---
+++ UrClass/Section_02/00_Sprint/fe-sprint-mini-node-server-master/server/basic-server.js
@@ -0,0 +1,40 @@
+// 모든 node 웹 서버 애플리케이션은 웹 서버 객체를 만들어야 합니다. 이 때 createServer 를 이용합니다.
+const http = require('http');
+const PORT = 4999;
+const ip = 'localhost';
+
+const server = http.createServer((request, response) => {
+  // * method, url, headers
+  const { method, url, headers } = request;
+
+  // * 요청 바디. -> 공식문서 그대로 긁어옴.
+  let body = [];
+  request
+    .on('data', (chunk) => {
+      body.push(chunk);
+    })
+    .on('end', () => {
+      body = Buffer.concat(body).toString();
+      // 여기서 `body`에 전체 요청 바디가 문자열로 담겨있습니다.
+    });
+
+  // * method 와 url 에 따라 분기.
+  // ? 만약에 메소드가 포스트이고 url 이 upper 이면 대문자를 리턴.
+  // ? 만약에 메소드가 포스트이고 url 이 lower 이면 소문자를 리턴.
+  // ? 나머지는 error 처리. bad request.
+
+  console.log(`http request method is ${request.method}, url is ${request.url}`);
+  response.writeHead(200, defaultCorsHeader);
+  response.end('hello mini-server sprints');
+});
+
+server.listen(PORT, ip, () => {
+  console.log(`http server listen on ${ip}:${PORT}`);
+});
+
+const defaultCorsHeader = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Accept',
+  'Access-Control-Max-Age': 10,
+};
