941ce6a3b281a28c2501b31d23d7a267d48549cb
82orez <82orez@naver.com>
2022-12-09 22:13:04 +0900

    refactoring ing2

--- UrClass/Section_02/00_Sprint/fe-sprint-mini-node-server-master/server/basic-server.js
+++ UrClass/Section_02/00_Sprint/fe-sprint-mini-node-server-master/server/basic-server.js
@@ -1,56 +1,34 @@
-// 모든 node 웹 서버 애플리케이션은 웹 서버 객체를 만들어야 합니다. 이 때 createServer 를 이용합니다.
-const http = require('http');
+const express = require('express')
+const cors = require('cors')
+
+const app = express(); //express 사용
+app.use(cors()); // 모든 요청/응답에 CORS 처리
+app.use(express.json({strict: false})) // 자동 바디 파싱
+
 const PORT = 4999;
 const ip = 'localhost';
 
-const server = http.createServer((request, response) => {
-  // * method, url, headers
-  const { method, url, headers } = request;
 
-  // * 요청 바디. -> 공식문서 그대로 긁어옴.
-  let body = [];
-  request
-    .on('data', (chunk) => {
-      body.push(chunk);
-    })
-    .on('end', () => {
-      body = Buffer.concat(body).toString();
+app.post('/upper', function (req, res){
+  let result = req.body;
+  result = result.toUpperCase();
+  res.json(result); // 다시 데이터를 보내줄 때 json 화 해서 보내주는 메소드
+})
+app.post('/lower', function (req, res){
+  let result = req.body;
+  result = result.toLowerCase();
+  res.json(result);
+})
 
-      // * 여기서 `body`에 전체 요청 바디가 문자열로 담겨있습니다.
-      //  * 공식 문서 '지금까지 살펴본 내용'
-
-      // * method 와 url 에 따라 분기.
-      // ? CORS 관련
-      //   헤더를 OPTIONS 응답에 적용해야 합니다.
-      //   클라이언트의 preflight request 에 대한 응답을 돌려줘야 합니다.
-      //   preflight request 에 대한 응답 헤더는 이미 작성되어 있습니다.
-
-      // ? 만약에 메소드가 포스트이고 url 이 upper 이면 대문자를 리턴.
-      // ? 만약에 메소드가 포스트이고 url 이 lower 이면 소문자를 리턴.
-      // ? 나머지는 error 처리. bad request.
-      if (request.method === 'OPTIONS') {
-        response.writeHead(200, defaultCorsHeader);
-        response.end();
-      } else if (request.method === 'POST' && request.url === '/upper') {
-        response.writeHead(200, defaultCorsHeader);
-        response.end(body.toUpperCase());
-      } else if (request.method === 'POST' && request.url === '/lower') {
-        response.writeHead(200, defaultCorsHeader);
-        response.end(body.toLowerCase());
-      } else {
-        response.statusCode = 404;
-        response.end('Bad Request');
-      }
-    });
+app.use(function(req, res, next) { // 400에러 즉, 클라이언트의 잘못으로 인한 에러는 인자에 err 를 넣지 않는다.
+  res.status(404).send('Something broke! 404');
 });
 
-server.listen(PORT, ip, () => {
+app.use(function(err, req, res, next) { //에러 처리
+  console.error(err.stack);
+  res.status(500).send('Something broke! 500');
+});
+
+app.listen(PORT, ip, () => {
   console.log(`http server listen on ${ip}:${PORT}`);
 });
-
-const defaultCorsHeader = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Accept',
-  'Access-Control-Max-Age': 10,
-};
