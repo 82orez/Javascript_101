8437726a1b3b87cb024ab9415e291ed6c4d15b1b
82orez <82orez@naver.com>
2022-12-12 10:32:59 +0900

    7 passed

--- UrClass/Section_02/00_Sprint/fe-sprint-statesairline-server-master/statesairline/controller/flightController.js
+++ UrClass/Section_02/00_Sprint/fe-sprint-statesairline-server-master/statesairline/controller/flightController.js
@@ -8,17 +8,51 @@ module.exports = {
   findAll: (req, res) => {
     const { departure_times, arrival_times, destination, departure } = req.query;
     // * TODO:
-    let newList;
 
+    // * filteredList 에 기존 flights 대입.
+    let filteredList = flights;
 
-    return res.json(flights);
+    // * query 에 departure_times 가 있으면, 조건문 실행.
+    if (departure_times) {
+      filteredList = filteredList.filter((value) => {
+        return departure_times === value.departure_times;
+      });
+    }
+    if (arrival_times) {
+      filteredList = filteredList.filter((value) => {
+        return arrival_times === value.arrival_times;
+      })
+    }
+    if (destination) {
+      filteredList = filteredList.filter((value) => {
+        return destination === value.destination;
+      })
+    }
+    if (departure) {
+      filteredList = filteredList.filter((value) => {
+        return departure === value.departure;
+      })
+    }
+
+    // return res.json(flights);
+    // * 필터된 배열 return.
+    return res.json(filteredList);
   },
+
   // ? [GET] /flight/:uuid
   // ? 요청 된 uuid 값과 동일한 uuid 값을 가진 항공편 데이터를 조회합니다.
   findById: (req, res) => {
     const { uuid } = req.params;
     // * TODO:
 
+    let filteredList = flights;
+
+    if (uuid) {
+      filteredList = filteredList.filter((value) => {
+        return uuid === value.uuid;
+      });
+    }
+    return res.json(filteredList);
   },
 
   // Advanced
--- UrClass/Section_02/00_Sprint/fe-sprint-statesairline-server-master/work-flow.js
+++ UrClass/Section_02/00_Sprint/fe-sprint-statesairline-server-master/work-flow.js
@@ -1,16 +1,31 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+// * 1. statesairline
+// ? statesairline/app.js 는 서비스에 필요한 미들웨어와 웹 서버를 실행하는 코드가 작성되어 있습니다.
+// const flightRouter = require('./router/flightRouter');
+// const bookRouter = require('./router/bookRouter');
+// const airportRouter = require('./router/airportRouter');
 
-// * 프로젝트를 실행하면 statesairline/app.js 파일이 실행된다.
+// * 2. router
+// ? statesairline/router/ 안에는 airport API, book API, flight API 요청을 수행하는 라우터가 작성되어 있습니다.
+// ? 작성된 라우터 내용을 통해 API 요청을 받을 수 있습니다.
 
-// * 이 파일의 내용은 다음과 같다.
-// *포트번호 3001 을 통해 req 를 받을 수 있는 서버를 실행한다.
+// * 3. controller
+// ? statesairline/controller/ 안에는 정의된 API 요청을 수행하는 코드를 작성해야 합니다.
+// ! ( airportController.js 에 작성된 내용을 참고하여 작성합니다. )
 
-// * 모든 요청에 대해 CORS 헤더를 붙인다.
-app.use(cors());
+// 4. repository
+// statesairline/repository/flightlist.js 는 서비스에서 제공하는 항공편 데이터가 작성되어 있습니다. 이 데이터로 서비스를 구현합니다.
+// statesairline/repository/airportlist.js 는 서비스에서 제공하는 공항 데이터가 작성되어 있습니다.
 
-// * 배열과 객체를 파싱해 준다.
-app.use(express.json());
+// 5.test
+// statesairline/__test__/statesairline.test.js 은 Jest 스펙을 가지고 있으며 코드 테스트를 위한 테스트 케이스가 작성되어 있습니다.
+// npm install 을 통해서 package.json 에 설정된 패키지를 설치하세요.
+// npm start 를 통해 서버를 실행하세요.
+// Postman 으로 API 동작 여부를 확인하세요.
+// npm test 를 통해 테스트를 실행하세요.
+// 서버에서 사용되는 포트 번호로 실행되지 않는 경우, 관리자 권한을 부여하여 실행하거나 해당 포트를 사용 중인 프로그램을 종료 후 재시작을 합니다.
 
-// * 미들웨어를 사용한다.
+// * Bare Minimum Requirements
+// ? statesairline/controller/flightController.js와 statesairline/controller/bookController.js 에 코드를 작성하세요.
+// ? Express 공식문서에서 req.query , req.params 를 사용하는 방법을 확인하세요. Query 와 Params 를 기준으로 데이터를 필터링하는 코드를 작성해야 합니다.
+// ? 예약 데이터는 controller/bookController.js 안에 작성된 let booking = []; 배열에 저장해야 합니다.
+// ? Flight API -> 항공편 수정은 Advanced Challenges 입니다.
