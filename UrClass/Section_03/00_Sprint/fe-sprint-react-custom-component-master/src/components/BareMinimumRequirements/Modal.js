69cf30bd7a5b0da84ac0f888812f51bebe8e15b2
82orez <82orez@naver.com>
2022-12-23 13:12:12 +0900

    ready for Custom Component 1.01

--- UrClass/Section_03/00_Sprint/fe-sprint-react-custom-component-master/src/components/BareMinimumRequirements/Modal.js
+++ UrClass/Section_03/00_Sprint/fe-sprint-react-custom-component-master/src/components/BareMinimumRequirements/Modal.js
@@ -7,7 +7,7 @@ export const ModalContainer = styled.div`
 `;
 
 export const ModalBackdrop = styled.div`
-  // TODO : Modal이 떴을 때의 배경을 깔아주는 CSS를 구현합니다.
+  // TODO : Modal 이 떴을 때의 배경을 깔아주는 CSS 를 구현합니다.
 `;
 
 export const ModalBtn = styled.button`
@@ -24,26 +24,26 @@ export const ModalView = styled.div.attrs((props) => ({
   // attrs 메소드를 이용해서 아래와 같이 div 엘리먼트에 속성을 추가할 수 있습니다.
   role: 'dialog',
 }))`
-  // TODO : Modal창 CSS를 구현합니다.
+  // TODO : Modal 창 CSS 를 구현합니다.
 `;
 
 export const Modal = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const openModalHandler = () => {
-    // TODO : isOpen의 상태를 변경하는 메소드를 구현합니다.
+    // TODO : isOpen 의 상태를 변경하는 메소드를 구현합니다.
   };
 
   return (
     <>
       <ModalContainer>
         <ModalBtn
-        // TODO : 클릭하면 Modal이 열린 상태(isOpen)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다.
+        // TODO : 클릭하면 Modal 이 열린 상태(isOpen)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다.
         >
           Open Modal
-          {/* TODO : 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때는 ModalBtn의 내부 텍스트가 'Opened!' 로 Modal이 닫힌 상태(isOpen이 false인 상태)일 때는 ModalBtn 의 내부 텍스트가 'Open Modal'이 되도록 구현해야 합니다. */}
+          {/* TODO : 조건부 렌더링을 활용해서 Modal 이 열린 상태(isOpen 이 true 인 상태)일 때는 ModalBtn 의 내부 텍스트가 'Opened!' 로 Modal 이 닫힌 상태(isOpen 이 false 인 상태)일 때는 ModalBtn 의 내부 텍스트가 'Open Modal' 이 되도록 구현해야 합니다. */}
         </ModalBtn>
-        {/* TODO : 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때만 모달창과 배경이 뜰 수 있게 구현해야 합니다. */}
+        {/* TODO : 조건부 렌더링을 활용해서 Modal 이 열린 상태(isOpen 이 true 인 상태)일 때만 모달창과 배경이 뜰 수 있게 구현해야 합니다. */}
       </ModalContainer>
     </>
   );
