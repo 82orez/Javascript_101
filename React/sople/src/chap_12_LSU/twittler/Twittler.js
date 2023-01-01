ae431552607372656873d9ee08f8f93291c61352
82orez <82orez@naver.com>
2023-01-02 04:37:23 +0900

    writer modified

--- React/sople/src/chap_12_LSU/twittler/Twittler.js
+++ React/sople/src/chap_12_LSU/twittler/Twittler.js
@@ -3,9 +3,8 @@ import NewTweetForm from './NewTweetForm';
 import SingleTweet from './SingleTweet';
 import './styles.css';
 
-const currentUser = 'TG';
-
 function Twittler() {
+  const [currentUser, setCurrentUser] = useState('TG');
   const [tweets, setTweets] = useState([
     {
       uuid: 1,
@@ -21,6 +20,10 @@ function Twittler() {
     },
   ]);
 
+  const handleChangeUser = (e) => {
+    setCurrentUser(e.target.value);
+  }
+
   // * 이 상태 변경 함수가 NewTweetForm 에 의해 실행되어야 합니다.
   const addNewTweet = (newTweet) => {
     // ? 전개연산자를 이용하면 기존 데이터를 변경하지 않아 불변성이 유지됨.
@@ -31,7 +34,11 @@ function Twittler() {
 
   return (
     <div>
-      <div>작성자: {currentUser}</div>
+      <label>
+        작성자:
+        <input type={'text'} value={currentUser} onChange={handleChangeUser}/>
+      </label>
+
       {/** 자식 컴포에 props 형태로 함수를 전달 **/}
       <NewTweetForm currentUser={currentUser} onButtonClick={addNewTweet} />
       <ul id="tweets">
