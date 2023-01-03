b412a0208ae8c763c5bb52c15fbb604d89d81913
82orez <82orez@naver.com>
2023-01-03 17:31:37 +0900

    modified

--- UrClass/00_Daily_coding/Section_02_Recursion/test.js
+++ UrClass/00_Daily_coding/Section_02_Recursion/test.js
@@ -1,43 +1,23 @@
-// * 두 개의 배열(base, sample)을 입력받아 sample 이 base 의 부분집합인지 여부를 리턴해야 합니다.
+function power(base, exponent) {
+  // todo: 여기에 코드를 작성합니다.
+  let num = 1;
 
-// ? boolean 타입을 리턴해야 합니다.
-// ? base, sample 내에 중복되는 요소는 없다고 가정합니다.
-
-const isSubsetOf = function (base, sample) {
-  // naive solution: O(M * N)
-  // return sample.every((item) => base.includes(item));
-
-  // 각 배열을 정렬: O(N * logN), O(M * logM)
-  // N >= M 이므로, O(N * logN)
-  base.sort((a, b) => a - b);
-  sample.sort((a, b) => a - b);
-
-  const findItemInSortedArr = (item, arr, from) => {
-    for (let i = from; i < arr.length; i++) {
-      if (item === arr[i]) return i;
-      else if (item < arr[i]) return -1;
-    }
-    return -1;
-  };
-
-  let baseIdx = 0;
-  for (let i = 0; i < sample.length; i++) {
-    baseIdx = findItemInSortedArr(sample[i], base, baseIdx);
-    if (baseIdx === -1) return false;
+  if (exponent === 0) {
+    return 1;
   }
-  return true;
-};
+  //
+  // return base * power(base, exponent - 1);
 
-let base = [1, 2, 3, 4, 5];
-let sample = [1, 3];
-let output = isSubsetOf(base, sample);
-console.log(output); // --> true
+  for (let i = 0; i < exponent; i++) {
+    num = num * base;
+  }
+  return num % 94906249;
+}
 
-sample = [6, 7];
-output = isSubsetOf(base, sample);
-console.log(output); // --> false
+let output = power(3, 40);
+console.log(output); // --> 19334827
 
-base = [10, 99, 123, 7];
-sample = [11, 100, 99, 123];
-output = isSubsetOf(base, sample);
-console.log(output); // --> false
+console.log(power(2, 0));
+console.log(power(2, 3));
+console.log(power(3, 3));
+console.log(power(5, 22));
