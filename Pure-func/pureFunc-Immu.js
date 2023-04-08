9c45d12adf42e9722fb412c8ed924af0c12f5f75
82orez <82orez@naver.com>
2023-04-08 10:49:23 +0900

    Pure func

---
+++ Pure-func/pureFunc-Immu.js
@@ -0,0 +1,33 @@
+// ? 아래 예는 무상태성, 불변성이 아님.
+const person = {
+    name: 'TG',
+    age: 32,
+};
+
+function increaseAge(person) {
+    person.age += 1;
+    return person;
+}
+
+increaseAge(person);
+
+// * 외부의 인자 person1 이라는 객체 자체에는 상태 변경이 생김.
+console.log('person: ', person);
+
+console.log('---------------------------------');
+
+// * Stateless(무상태성), Immutability(불변성) => O
+const person1 = {
+    name: 'TG',
+    age: 32,
+};
+
+function increaseAge1(person1) {
+    // * 외부의 인자(객체)를 입력 받아 새로운 객체를 반환 => Immutability(붋변성)
+    return { ...person1, age: person1.age + 1 };
+}
+
+console.log(increaseAge1(person1));
+
+// * 외부의 인자 person1 이라는 객체 자체에는 상태 변경이 없음.
+console.log('person1: ', person1);
---
+++ Pure-func/pureFunc.js
@@ -0,0 +1,18 @@
+// ? 아래 예는 순수 함수가 아님.
+// ? 외부 변수 num 의 값이 변함에 따라 함수의 결과값이 달라짐.
+let num =1;
+
+function add(a) {
+    return a + num;
+}
+
+console.log(add(1));
+
+
+// * 아래 함수는 순수 함수임.
+// * 외부 변수에 의해 함수의 결과값이 달라지지 않고 항상 같은 결과값을 가짐.
+function add1(a, b) {
+    return a + b;
+}
+
+console.log(add1(1, 2));
