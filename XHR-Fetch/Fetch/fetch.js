9bde05be0cd32e901d22cb4f3bf60f4c24b0d8c3
82orez <82orez@naver.com>
2023-06-09 09:41:27 +0900

    fetch modified

--- XHR-Fetch/Fetch/fetch.js
+++ XHR-Fetch/Fetch/fetch.js
@@ -1,6 +1,6 @@
 // * fetch 는 Promise 를 반환한다는 것이 XHR 과 가장 큰 차이점이다.
 // ? url 다음 자리에 옵션이 생략되면 method 는 get 이다.
-const json = fetch('https://reqres.in/api/products/10');
+const json = fetch('https://reqres.in/api/products/');
 
 // Promise 를 반환.
 console.log(json);
@@ -8,4 +8,4 @@ console.log();
 
 json
   .then(re => re.json())    // * fetch 를 통해 받아온 JSON 문자열을 객체로 변환.
-  .then(obj => console.log(obj.data.id));
+  .then(obj => console.log(obj.data[0].id));
